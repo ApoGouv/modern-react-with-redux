a67c9722d193319db9eafb1e0a811634b1c729cc
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-10 12:00:51 +0300

    fetching data to our components via props

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -7,8 +7,25 @@
 
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>;
+const VideoListItem = ({video}) => {
+  // const video = props.video; ~~ to {video} above
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-list">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
