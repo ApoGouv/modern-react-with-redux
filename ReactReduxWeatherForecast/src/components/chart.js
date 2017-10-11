43ab1fa461a7813b7a7a8e909cc967f2940e4d09
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-11 20:40:32 +0300

    Add SparkLine for charts of temperature, pressure and humidity

---
+++ ReactReduxWeatherForecast/src/components/chart.js
@@ -0,0 +1,18 @@
+/**
+ * File    : chart.js
+ * Project : modern-react-with-redux
+ * Author  : Apostolos Gouvalas
+ * Date    : 11/10/2017
+ */
+import React from 'react';
+import {Sparklines, SparklinesLine } from 'react-sparklines';
+
+export default (props) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
+      </Sparklines>
+    </div>
+  );
+}
--- ReactReduxWeatherForecast/src/containers/weather_list.js
+++ ReactReduxWeatherForecast/src/containers/weather_list.js
@@ -5,19 +5,32 @@
  * Date    : 11/10/2017
  */
 import React, {Component} from 'react';
-
 import {connect} from 'react-redux';
+
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
+  /**
+   * renders a single row
+   * @param cityData
+   * @returns {XML}
+   */
   renderWeather(cityData) {
     const name = cityData.city.name;
+    // array of temperatures for 5 days for a given city
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>
           {name}
         </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="green" /></td>
+        <td><Chart data={humidities} color="black" /></td>
       </tr>
     );
   }
