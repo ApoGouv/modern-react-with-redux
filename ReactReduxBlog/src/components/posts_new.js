d61c286a8f2f0fc0f8a8f981415d97ab161f38dc
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-12 14:23:20 +0300

    Add new route /posts/new

---
+++ ReactReduxBlog/src/components/posts_new.js
@@ -0,0 +1,19 @@
+/**
+ * File    : posts_new.js
+ * Project : modern-react-with-redux
+ * Author  : Apostolos Gouvalas
+ * Date    : 12/10/2017
+ */
+import React, {Component} from 'react';
+
+class PostsNew extends Component {
+  render(){
+    return (
+      <div>
+        <h3>Add a New Post</h3>
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
